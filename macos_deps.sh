30574cbbea27ccfc09ebe17ebaa48ebf182c8599
ZChuckMoris <18584322+ZChuckMoris@users.noreply.github.com>
2022-01-25 19:56:29 +0300

    fix brew elevation after install

--- macos_deps.sh
+++ macos_deps.sh
@@ -8,7 +8,8 @@ fi;
 #Install brew if not installed (else update brew)
 if [[ $(command -v brew) == "" ]]; then 
     echo "Installing Homebrew"; 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; 
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else 
     echo "Updating Homebrew"; 
     brew update; 
